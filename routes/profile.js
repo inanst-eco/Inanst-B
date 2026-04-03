afd8838e0b0e9b2dd462a26acd52f2c5193b43cf
Wasem1209 <philipwasem3@gmail.com>
2026-04-03 07:02:01 +0100

    Corrected the middleware importation

--- routes/profile.js
+++ routes/profile.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { getUserProfile } = require('../controllers/UserController');
-const { protect } = require('../middleware/authMiddleware'); 
+const { protect } = require('../middleware/auth.js'); 
 
 // The route to handled by the controller
 router.get('/', protect, getUserProfile);
