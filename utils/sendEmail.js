3fd650fae57cfa4e4e6ba4c4bd13d24ba5283ffb
Wasem1209 <philipwasem3@gmail.com>
2026-04-13 07:43:01 +0100

    Update on the auth

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,19 +2,17 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = async (to, subject, html) => {
     
-    const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 587,
-        secure: false, 
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS,
-        },
-       
-        family: 4, 
-        connectionTimeout: 10000,
-    });
-
+   const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, 
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    },
+    family: 4, 
+   });
+    
     try {
         await transporter.sendMail({
             from: `"Inanst" <${process.env.EMAIL_USER}>`,
