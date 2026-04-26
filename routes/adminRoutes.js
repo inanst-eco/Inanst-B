76b9575f9779bbe239d015185b1d305126d9374f
Wasem1209 <philipwasem3@gmail.com>
2026-04-26 06:43:47 +0100

    God

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,13 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const adminController = require('../controllers/adminController');
-const authMiddleware = require('../middleware/auth.js');
-
-const { getAdminOversightStats, updateUserRole, deleteUser } = adminController;
-const { protect, adminOnly } = authMiddleware;
-
-router.get('/oversight-stats', protect, adminOnly, getAdminOversightStats);
-router.patch('/update-role', protect, adminOnly, updateUserRole);
-router.delete('/user/:userId', protect, adminOnly, deleteUser); // Added Delete Route
+const { getAdminOversightStats } = require('../controllers/adminController');
+const { protect, admin } = require('../middleware/authMiddleware'); 
+router.get('/oversight-stats', protect, admin, getAdminOversightStats);
 
 module.exports = router;
