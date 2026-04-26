c67eaa8a5ae98a6e114dea93cc538f1c6d1f60b3
Wasem1209 <philipwasem3@gmail.com>
2026-04-26 07:27:22 +0100

    Update on the admin

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
+
 const { getAdminOversightStats } = require('../controllers/adminController');
-const { protect, admin } = require('../middleware/auth'); 
-router.get('/oversight-stats', protect, admin, getAdminOversightStats);
+
+const { protect, adminOnly } = require('../middleware/auth'); 
+
+
+router.get('/oversight-stats', protect, adminOnly, getAdminOversightStats);
 
 module.exports = router;
