343978c7a2c9d87801305968af7348876be2ed46
Wasem1209 <philipwasem3@gmail.com>
2026-04-27 09:38:22 +0100

    created api for service

--- models/School.js
+++ models/School.js
@@ -1,10 +1,24 @@
 const mongoose = require('mongoose');
 
-const SchoolSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    description: { type: String, required: true },
-    price: { type: String, required: true }, 
-    iconName: { type: String, required: true },
+const ServiceSchema = new mongoose.Schema({
+    title: { 
+        type: String, 
+        required: [true, 'Please add a service title'],
+        unique: true,
+        trim: true
+    },
+    description: { 
+        type: String, 
+        required: [true, 'Please add a description'] 
+    },
+    iconName: { 
+        type: String, 
+        required: [true, 'Please specify a Lucide icon name'] 
+    },
+    actionLink: { 
+        type: String, 
+        default: '/regular/service/request' 
+    }
 }, { timestamps: true });
 
-module.exports = mongoose.model('School', SchoolSchema);
+module.exports = mongoose.model('Service', ServiceSchema);
